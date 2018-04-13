c73b6ad245e6820872c542fabf9108da6e8747b5
Zachary Aten <z_man@Zacharys-MBP.hsd1.ga.comcast.net>
2018-04-13 09:50:11 -0400

    changed app to a class component to manage state

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBND_nr8gqFs5zMaCOtL-KiaPwOsmHD6LA';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-    console.log(data);
-});
+class App extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            videos: []
+        };
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+            // same thing as this.setState({ videos: videos });
+        });
+    }
+    
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
     );
-};
+}
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
